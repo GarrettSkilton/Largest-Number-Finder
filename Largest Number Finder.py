87558c8c6e46f69f01c14e87637289a6602da262
GarrettSkilton <128932204+GarrettSkilton@users.noreply.github.com>
2023-04-03 15:36:36 -0500

    Update Largest Number Finder.py

--- Largest Number Finder.py
+++ Largest Number Finder.py
@@ -3,4 +3,4 @@ largest_so_far = -1
 for the_num in [10, 14, 20, 55, 743, 2, 1123, 123, 22, 4234, 4, 232, 2323 , 44156, 75] :
     if the_num > largest_so_far :
         largest_so_far = the_num
-print('The largest number in this dataset is ' , largest_so_far)
+print('The largest number in this dataset is ', largest_so_far)
